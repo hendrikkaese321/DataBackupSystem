3eb0d34fec198fbf5fbb6e2d20b7377ba107eb0d
hendrikkaese321 <58625304+hendrikkaese321@users.noreply.github.com>
2024-06-02 13:22:39 +0300

    enhance readability

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const apiRoutes = require('./routes');
-
+const apiRoutes = require('./rndirectory to your routes fileoutes');
 const server = express();
-
 server.use(bodyParser.json());
-
-mongoose.connect(process.env.DATABASE_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connection established successfully.'))
-  .catch(err => console.error('Failed to connect to MongoDB: ', err));
-
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connection established successfully.'))
+.catch(err => console.error('Failed to connect to MongoDB: ', err));
 server.use('/api', apiRoutes);
-
 const SERVER_PORT = process.env.PORT || 3000;
 server.listen(SERVER_PORT, () => {
   console.log(`Server is up and running on port ${SERVER_PORT}.`);
